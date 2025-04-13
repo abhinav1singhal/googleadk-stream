100b80dc9297871d65b1776f18ae2a85720b8a45
abhinav1singhal <abhinav1singhal@gmail.com>
2025-04-13 18:31:50 -0400

    update the gemini model
    Gemini Model updated

--- app/google_search_agent/agent.py
+++ app/google_search_agent/agent.py
@@ -5,7 +5,7 @@ root_agent=Agent(
     #unique name of agent
     name="first_searc_agent",
     #LLM I want to use, make sure you check the name if llm
-    model="gemini-2.0-flash-live-001", # Google AI Studio
+    model="gemini-2.0-flash", # Google AI Studio
     #short descrition of models purpose
     description="Google search using this agent",
     #instructions to set Agent's behaviour
@@ -13,5 +13,4 @@ root_agent=Agent(
     #Add the tools for this agent. In this case we will use google_search as tool. This is a prebuult tool
     #you can create your our tool as well to act as a subagent
     tools=[google_search]
-
 )
